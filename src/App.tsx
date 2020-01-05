c7195f8df223fd02e88a6758fed47c5fb3a5bb8a
Kyle Tolle <kyle@nullsix.com>
2020-01-04 22:43:23 -0700

    Make the minutes input text larger

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ class TimerInput extends React.Component<TimerInputProps, TimerInputState> {
       <div>
         <h3>Input your desired time</h3>
         <input 
+          style={{ fontSize: '2em', width: '2em' }}
           type="number"
           min="0"
           disabled={this.props.disabled}
