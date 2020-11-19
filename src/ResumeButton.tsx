300344d71f7acf191da704a4422ac7f2330aac1c
Kyle Tolle <kyle@nullsix.com>
2020-11-19 11:19:34 -0700

    Use emotion styles in ResumeButton

--- src/ResumeButton.tsx
+++ src/ResumeButton.tsx
@@ -1,16 +1,29 @@
+/** @jsx jsx */
 import React from 'react';
-import { ResumeButtonProps } from './ResumeButtonProps';
+import { jsx, css } from '@emotion/react'
+import styled from '@emotion/styled'
+import ResumeButtonProps from './ResumeButtonProps';
 
-export class ResumeButton extends React.Component<ResumeButtonProps> {
-  render() {
-    let styleAttrs = {
-      marginLeft: 50,
-      display: this.props.visible ? 'inline-block' : 'none'
-    };
-    return (<div style={styleAttrs}>
-      <button style={{ fontSize: 50 }} onClick={this.props.handleClick}>
-        Resume
-        </button>
-    </div>);
-  }
+const display = (props: ResumeButtonProps) => (
+  css`
+    display: ${props.visible ? 'inline-block' : 'none'},
+  `
+);
+
+const UnstyledResumeButton: React.FC<ResumeButtonProps> = ({ className, handleClick }) => {
+  return (<div className={className}>
+    <button onClick={handleClick}>
+      Resume
+      </button>
+  </div>);
 }
+
+const ResumeButton = styled(UnstyledResumeButton)`
+  ${display}
+
+  button {
+    font-size: 50;
+  }
+`
+
+export default ResumeButton;
